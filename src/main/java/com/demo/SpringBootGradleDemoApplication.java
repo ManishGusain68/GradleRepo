069491c988c33bc2047e5ad50455adf6e5ae63ec
ManishGusain <gusainmanish68@gmail.com>
2022-05-11 09:47:12 +0530

    ff

--- src/main/java/com/demo/SpringBootGradleDemoApplication.java
+++ src/main/java/com/demo/SpringBootGradleDemoApplication.java
@@ -10,20 +10,18 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import static springfox.documentation.builders.PathSelectors.regex;
+
 @SpringBootApplication
-<<<<<<< HEAD
+
 @EnableSwagger2
 
-=======
-@EnableSwagger2 
 //enabled swagger here
->>>>>>> dev1
+
 public class SpringBootGradleDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGradleDemoApplication.class, args);
 	}
-	//it is a gradle prject to demo and learn
-	
+	// it is a gradle prject to demo and learn
 
 }
